bfbe95ba4b235e3b7f9e0a0be1ceb44ed81e5b36
LastFoxTrail <liza.lis.2505@mail.ru>
2023-10-24 18:52:57 +0300

    clear master

--- src/main/java/com/bashlacheva/lab2/Main.java
+++
@@ -1,9 +0,0 @@
-package com.bashlacheva.lab2;
-
-public class Main {
-	
-    public static void main(String[] args) {
-        Annotated method = new Annotated();
-        MethodCaller.callAnnotated(method);
-    }
-}
