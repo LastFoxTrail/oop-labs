8d68cb52e184253e091f1173439db992279c4f90
LastFoxTrail <liza.lis.2505@mail.ru>
2023-10-24 18:26:09 +0300

    add lab2 source files

---
+++ src/main/java/com/bashlacheva/lab2/Main.java
@@ -0,0 +1,9 @@
+package com.bashlacheva.lab2;
+
+public class Main {
+	
+    public static void main(String[] args) {
+        Annotated method = new Annotated();
+        MethodCaller.callAnnotated(method);
+    }
+}
