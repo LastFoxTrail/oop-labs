6392c124dcaa3870fb7f0dfedae8e4bcc5aca715
LastFoxTrail <liza.lis.2505@mail.ru>
2023-10-25 11:26:13 +0300

    add main point

---
+++ src/main/java/com/bashlacheva/lab5/Main.java
@@ -0,0 +1,10 @@
+package main.java.com.bashlacheva.lab5;
+
+import main.java.com.bashlacheva.lab5.test.Test;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Test.run();
+	}
+}
